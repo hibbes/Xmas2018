50cb8f57489659621582617b5a1492f7b89c8696
Marek.Czernohous <Marek.Czernohous@DESKTOP-7H44MPR.2631.skool.net>
2019-12-13 10:07:20 +0100

    Christbaumkugeln

--- src/Xmas2018.java
+++ src/Xmas2018.java
@@ -1,11 +1,17 @@
+import java.util.Random;
 
 public class Xmas2018 {
  
  
 	public static void main(String[] args) {
-		int breite = 11;  // Zeichenbreite des Baums an dickster Stelle
+		
+		int breite = 100;  // Zeichenbreite des Baums an dickster Stelle
 		int count = 1;    // Zähler für Ausgabe der Blätter (2. FOR-Schleife)
-	
+		Random random = new Random(); // Zufallszahl-Objekt erstellen
+		int zufallszahl; // Zufallszahl-Variable deklarieren
+	    int stamm = breite; // Breite speichern für Stammberechnung
+	    
+	    
 	while(breite>0){       
 		for(int i = 0; i<breite/2; i++){   	// Die Schleife für die Leerzeichen 
 											//in jeder Zeile, läuft, solange der Wert < 
@@ -15,8 +21,11 @@ public class Xmas2018 {
 		breite--;							// nach jeder Zeile wird ein Leerzeichen weniger ausgegeben 
 											// Vergleichswert "Breite" -1
 		
-		for(int i = 0; i<count; i++){		// Schleife für Blätter, Anzahl steigt mit jedem Durchlauf.
-			System.out.print("@");
+		for(int i = 0; i<count; i++){ 	// Schleife für Blätter, Anzahl steigt mit jedem Durchlauf.
+			zufallszahl= 1 + random.nextInt(7); // Zufallszahl zwischen 0 und 7, für Christbaumschmuck
+	    	  if(zufallszahl%7==0){ // Wenn Zahl == 0, dann Christbaumkugel
+	    		  System.out.print("@");}
+	    	  	else{System.out.print("X");}  // Sonst Tannennadeln
 		}
 		System.out.println();
 		count++;							// Counter +1, damit die Anzahl der Blätter in jeder Zeile steigt. 
@@ -27,8 +36,11 @@ public class Xmas2018 {
 		for(int i= 0; i<(count/2)-1;i++){
 			System.out.print(" ");
 		}
-		for(int i = 0; i<2; i++){		// Schleife für Blätter, Anzahl steigt mit jedem Durchlauf.
-			System.out.print("@");
+		
+		if(count%2==0){count = 2;}else{count = 3;}
+		
+		for(int i = 0; i<count; i++){		// Schleife für Stamm
+			System.out.print("|");
 		}
 		
 		
